82c440639b1ee1f36d326b622c161b0705dd362c
cassie <zhangm9@seattleu.edu>
2018-04-29 17:11:16 -0700

    hw2prepfiles

---
+++ hw2prepfiles/empdata_zhang.js
@@ -0,0 +1,40 @@
+{
+  "data": [
+    {
+      "name": "Mengyao Zhang",
+      "position": "Buiness Analyst",
+      "salary": "$100,000",
+      "city": "Seattle",
+      "email": "zhangm9@seattleu.edu"
+    },
+    {
+      "name": "Jiayang He",
+      "position": "Senior Engineer",
+      "salary": "$210,000",
+      "city": "Seattle",
+      "email": "hek1029@hotmail.com"
+    },
+    {
+      "name": "Qi Jiang",
+      "position": "Accountant",
+      "salary": "$90,000",
+      "city": "Portland",
+      "email": "jiang1234@hotmail.com"
+    },
+    {
+      "name": "Junfan Tian",
+      "position": "Designer",
+      "salary": "$250,000",
+      "city": "Boston",
+      "email": "tianjun12@uw.edu"
+    },
+    {
+      "name": "Mia Dunglas",
+      "position": "Photographer",
+      "salary": "$99,000",
+      "city": "Paris",
+      "email": "miaww123@hotmail.com"
+
+    }
+  ]
+}
